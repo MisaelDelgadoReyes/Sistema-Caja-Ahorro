9611a710c8cb1ddc04b7d1bffd17ba944d9429c0
MisaelDelgadoReyes <misacraftreynob@gmail.com>
2026-06-16 15:25:20 -0500

    Estructura base inicial de Spring Boot

---
+++ caja-ahorro/src/main/java/com/grupo5/caja_ahorro/CajaAhorroApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.caja_ahorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajaAhorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajaAhorroApplication.class, args);
+	}
+
+}
