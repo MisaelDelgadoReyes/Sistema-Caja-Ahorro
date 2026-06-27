46ac7e8f8085a8a5bc5d41d0de6f9bdbd33620ff
MisaelDelgadoReyes <misacraftreynob@gmail.com>
2026-06-27 18:32:00 -0500

    Test-credito

--- src/main/java/com/grupo5/caja_ahorro/model/Credito.java
+++ src/main/java/com/grupo5/caja_ahorro/model/Credito.java
@@ -1,12 +1,23 @@
 package com.grupo5.caja_ahorro.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "creditos")
